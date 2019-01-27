b52499f6bd3a8fc846dd0363b1244d917e673da0
Aaron <aarongunther97@gmail.com>
2019-01-27 11:55:11 -0600

    wrote to a file

--- hw1/shift_cipher_decrypt.py
+++ hw1/shift_cipher_decrypt.py
@@ -1,13 +1,15 @@
 from itertools import count
 from string import ascii_lowercase
 
-# cipher_text = open('shift_cipher_text.txt', 'r')
-cipher_text = 'beeakfydjxuqyhyjiqryhtyjiqfbqduyjiikfuhcqd' # ddurq
-letters = dict(zip(ascii_lowercase, count(0)))
-numbers = dict(zip(count(0), ascii_lowercase))
-cipher_text_numbers = [letters[letter] for letter in cipher_text]
+cipher_text = 'beeakfydjxuqyhyjiqryhtyjiqfbqduyjiikfuhcqd'
+results_file = open('results.txt', 'w')
+letters_to_numbers = dict(zip(ascii_lowercase, count(0)))
+numbers_to_letters = dict(zip(count(0), ascii_lowercase))
+cipher_text_numbers = [letters_to_numbers[letter] for letter in cipher_text]
 for i in range(1,26):
     shifted_cipher = [(num - i)%26 for num in cipher_text_numbers] 
-    plain_text = [numbers[num] for num in shifted_cipher]
-    print('Shift Amount: ' + str(i) + ':')
-    print(''.join(plain_text))
+    plain_text = [numbers_to_letters[num] for num in shifted_cipher]
+    results_file.write('\n')
+    results_file.write('Shift Amount: ' + str(i))
+    results_file.write('\n')
+    results_file.write(''.join(plain_text))
